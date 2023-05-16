a5b566f6e4669d17cdc07bb194ae65c6542804a1
sedat.akkus <sedat.akkus@base86.net>
2023-05-16 21:23:58 +0300

    feat: add good cheap fast page

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Good = new System.Windows.Forms.Button();
+            this.Cheap = new System.Windows.Forms.Button();
+            this.Fast = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Good
+            // 
+            this.Good.BackColor = System.Drawing.Color.Brown;
+            this.Good.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Good.ForeColor = System.Drawing.Color.White;
+            this.Good.Location = new System.Drawing.Point(388, 12);
+            this.Good.Name = "Good";
+            this.Good.Size = new System.Drawing.Size(284, 128);
+            this.Good.TabIndex = 0;
+            this.Good.Text = "Good";
+            this.Good.UseVisualStyleBackColor = false;
+            this.Good.Click += new System.EventHandler(this.Good_Click);
+            // 
+            // Cheap
+            // 
+            this.Cheap.BackColor = System.Drawing.Color.Brown;
+            this.Cheap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Cheap.ForeColor = System.Drawing.Color.White;
+            this.Cheap.Location = new System.Drawing.Point(388, 146);
+            this.Cheap.Name = "Cheap";
+            this.Cheap.Size = new System.Drawing.Size(284, 128);
+            this.Cheap.TabIndex = 1;
+            this.Cheap.Text = "Cheap";
+            this.Cheap.UseVisualStyleBackColor = false;
+            this.Cheap.Click += new System.EventHandler(this.Cheap_Click);
+            // 
+            // Fast
+            // 
+            this.Fast.BackColor = System.Drawing.Color.Brown;
+            this.Fast.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Fast.ForeColor = System.Drawing.Color.White;
+            this.Fast.Location = new System.Drawing.Point(388, 280);
+            this.Fast.Name = "Fast";
+            this.Fast.Size = new System.Drawing.Size(284, 128);
+            this.Fast.TabIndex = 2;
+            this.Fast.Text = "Fast";
+            this.Fast.UseVisualStyleBackColor = false;
+            this.Fast.Click += new System.EventHandler(this.Fast_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1156, 418);
+            this.Controls.Add(this.Fast);
+            this.Controls.Add(this.Cheap);
+            this.Controls.Add(this.Good);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button Good;
+        private Button Cheap;
+        private Button Fast;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -16,5 +16,75 @@ namespace MyFirstFormAppProject
         {
             InitializeComponent();
         }
+        enum Durum { SeciliDegil, Secili } // bool
+        Durum GoodButtonDurum = Durum.SeciliDegil;
+        Durum CheapButtonDurum = Durum.SeciliDegil;
+        Durum FastButtonDurum = Durum.SeciliDegil;
+
+        private void Good_Click(object sender, EventArgs e)
+        {
+            if (GoodButtonDurum == Durum.SeciliDegil)//seciliyse
+            {
+                GoodButtonDurum = Durum.Secili;
+                if (CheapButtonDurum == Durum.Secili)
+                {
+                    FastButtonDurum = Durum.SeciliDegil;
+                    RenkDegistir(Fast, FastButtonDurum);
+                }
+            }
+            else if (GoodButtonDurum == Durum.Secili)//secilideğilse
+            {
+                GoodButtonDurum = Durum.SeciliDegil;
+            }
+
+            RenkDegistir(Good, GoodButtonDurum);
+        }
+
+        private void Cheap_Click(object sender, EventArgs e)
+        {
+            if (CheapButtonDurum == Durum.SeciliDegil)//seciliyse
+            {
+                CheapButtonDurum = Durum.Secili;
+                if (FastButtonDurum == Durum.Secili)
+                {
+                    GoodButtonDurum = Durum.SeciliDegil;
+                    RenkDegistir(Good, GoodButtonDurum);
+                }
+            }
+            else if (CheapButtonDurum == Durum.Secili)//secilideğilse
+            {
+                CheapButtonDurum = Durum.SeciliDegil;
+            }
+
+            RenkDegistir(Cheap, CheapButtonDurum);
+        }
+
+        private void Fast_Click(object sender, EventArgs e)
+        {
+            if (FastButtonDurum == Durum.SeciliDegil)//seciliyse
+            {
+                FastButtonDurum = Durum.Secili;
+                if (GoodButtonDurum == Durum.Secili)
+                {
+                    CheapButtonDurum = Durum.SeciliDegil;
+                    RenkDegistir(Cheap, CheapButtonDurum);
+                }
+            }
+            else if (FastButtonDurum == Durum.Secili)//secilideğilse
+            {
+                FastButtonDurum = Durum.SeciliDegil;
+            }
+
+            RenkDegistir(Fast, FastButtonDurum);
+        }
+
+
+        void RenkDegistir(Button button,Durum buttonDurum)
+        {
+            button.BackColor = (buttonDurum == Durum.Secili) ? Color.YellowGreen : Color.Brown;
+        }
+
+        
+
     }
 }
