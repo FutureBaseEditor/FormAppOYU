c7f51af5f2e641970a867d4f95e036c75dd41fb2
sedat.akkus <sedat@base86.net>
2023-05-23 19:32:21 +0300

    for mert

---
+++ Form5.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace MyFirstFormAppProject
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.suhrab = new System.Windows.Forms.PictureBox();
+            this.dila = new System.Windows.Forms.PictureBox();
+            this.lokman = new System.Windows.Forms.PictureBox();
+            this.alpay = new System.Windows.Forms.PictureBox();
+            this.mert = new System.Windows.Forms.PictureBox();
+            this.mustafa = new System.Windows.Forms.PictureBox();
+            this.senan = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.finish = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.hole = new System.Windows.Forms.PictureBox();
+            this.bonus = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.suhrab)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dila)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lokman)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alpay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mert)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mustafa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.senan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.finish)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hole)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bonus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // suhrab
+            // 
+            this.suhrab.Image = ((System.Drawing.Image)(resources.GetObject("suhrab.Image")));
+            this.suhrab.Location = new System.Drawing.Point(12, 12);
+            this.suhrab.Name = "suhrab";
+            this.suhrab.Size = new System.Drawing.Size(100, 100);
+            this.suhrab.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.suhrab.TabIndex = 2;
+            this.suhrab.TabStop = false;
+            this.suhrab.LocationChanged += new System.EventHandler(this.suhrab_LocationChanged);
+            // 
+            // dila
+            // 
+            this.dila.Image = ((System.Drawing.Image)(resources.GetObject("dila.Image")));
+            this.dila.Location = new System.Drawing.Point(12, 118);
+            this.dila.Name = "dila";
+            this.dila.Size = new System.Drawing.Size(100, 100);
+            this.dila.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dila.TabIndex = 3;
+            this.dila.TabStop = false;
+            this.dila.LocationChanged += new System.EventHandler(this.dila_LocationChanged);
+            // 
+            // lokman
+            // 
+            this.lokman.Image = ((System.Drawing.Image)(resources.GetObject("lokman.Image")));
+            this.lokman.Location = new System.Drawing.Point(12, 224);
+            this.lokman.Name = "lokman";
+            this.lokman.Size = new System.Drawing.Size(100, 100);
+            this.lokman.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lokman.TabIndex = 4;
+            this.lokman.TabStop = false;
+            // 
+            // alpay
+            // 
+            this.alpay.Image = ((System.Drawing.Image)(resources.GetObject("alpay.Image")));
+            this.alpay.Location = new System.Drawing.Point(12, 542);
+            this.alpay.Name = "alpay";
+            this.alpay.Size = new System.Drawing.Size(100, 100);
+            this.alpay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.alpay.TabIndex = 5;
+            this.alpay.TabStop = false;
+            // 
+            // mert
+            // 
+            this.mert.Image = ((System.Drawing.Image)(resources.GetObject("mert.Image")));
+            this.mert.Location = new System.Drawing.Point(12, 436);
+            this.mert.Name = "mert";
+            this.mert.Size = new System.Drawing.Size(100, 100);
+            this.mert.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mert.TabIndex = 6;
+            this.mert.TabStop = false;
+            // 
+            // mustafa
+            // 
+            this.mustafa.Image = ((System.Drawing.Image)(resources.GetObject("mustafa.Image")));
+            this.mustafa.Location = new System.Drawing.Point(12, 330);
+            this.mustafa.Name = "mustafa";
+            this.mustafa.Size = new System.Drawing.Size(100, 100);
+            this.mustafa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mustafa.TabIndex = 7;
+            this.mustafa.TabStop = false;
+            // 
+            // senan
+            // 
+            this.senan.Image = ((System.Drawing.Image)(resources.GetObject("senan.Image")));
+            this.senan.Location = new System.Drawing.Point(12, 648);
+            this.senan.Name = "senan";
+            this.senan.Size = new System.Drawing.Size(100, 100);
+            this.senan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.senan.TabIndex = 8;
+            this.senan.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 787);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // finish
+            // 
+            this.finish.Image = ((System.Drawing.Image)(resources.GetObject("finish.Image")));
+            this.finish.Location = new System.Drawing.Point(1213, 12);
+            this.finish.Name = "finish";
+            this.finish.Size = new System.Drawing.Size(153, 156);
+            this.finish.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.finish.TabIndex = 10;
+            this.finish.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(1213, 174);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(153, 156);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(1213, 336);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(153, 156);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(1213, 498);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(153, 156);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 13;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(1213, 660);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(153, 156);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 14;
+            this.pictureBox6.TabStop = false;
+            // 
+            // hole
+            // 
+            this.hole.Image = ((System.Drawing.Image)(resources.GetObject("hole.Image")));
+            this.hole.Location = new System.Drawing.Point(801, 12);
+            this.hole.Name = "hole";
+            this.hole.Size = new System.Drawing.Size(95, 95);
+            this.hole.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hole.TabIndex = 15;
+            this.hole.TabStop = false;
+            this.hole.Visible = false;
+            // 
+            // bonus
+            // 
+            this.bonus.Image = ((System.Drawing.Image)(resources.GetObject("bonus.Image")));
+            this.bonus.Location = new System.Drawing.Point(700, 12);
+            this.bonus.Name = "bonus";
+            this.bonus.Size = new System.Drawing.Size(95, 95);
+            this.bonus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bonus.TabIndex = 16;
+            this.bonus.TabStop = false;
+            this.bonus.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1378, 829);
+            this.Controls.Add(this.bonus);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.senan);
+            this.Controls.Add(this.mustafa);
+            this.Controls.Add(this.mert);
+            this.Controls.Add(this.alpay);
+            this.Controls.Add(this.lokman);
+            this.Controls.Add(this.dila);
+            this.Controls.Add(this.suhrab);
+            this.Controls.Add(this.hole);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.finish);
+            this.Name = "Form5";
+            this.Text = "Alpay Hocaaaa    ";
+            ((System.ComponentModel.ISupportInitialize)(this.suhrab)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dila)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lokman)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alpay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mert)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mustafa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.senan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.finish)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hole)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bonus)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox suhrab;
+        private PictureBox dila;
+        private PictureBox lokman;
+        private PictureBox alpay;
+        private PictureBox mert;
+        private PictureBox mustafa;
+        private PictureBox senan;
+        private Button button1;
+        private PictureBox finish;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox hole;
+        private PictureBox bonus;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ Form5.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace MyFirstFormAppProject
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+        
+        Random rnd = new Random();
+        int tickCount = 0;
+        int randomTime = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            if (tickCount == 0)
+            {
+                randomTime = rnd.Next(25);
+            }
+
+            if (tickCount == randomTime)
+            {
+                hole.Visible = true;
+                bonus.Visible = true;
+                int holeX = rnd.Next(this.Size.Width - hole.Width - finish.Width);
+                int holeY = rnd.Next(this.Size.Height - hole.Height);
+                hole.Location = new Point(holeX, holeY);
+
+                int bonusX = rnd.Next(this.Size.Width - bonus.Width - finish.Width);
+                int bonusY = rnd.Next(this.Size.Height - bonus.Height);
+                bonus.Location = new Point(bonusX, bonusY);
+
+                tickCount = 0;
+            }
+
+            tickCount++;
+
+            Go(suhrab);
+            Go(dila);
+            Go(lokman);
+            Go(mustafa);
+            Go(mert);
+            Go(alpay);
+            Go(senan);
+        }
+
+        void Go(PictureBox pb)
+        {
+            int value = rnd.Next(25);
+            pb.Location = new Point(pb.Location.X + value, pb.Location.Y);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer2.Start();
+        }
+
+        private void suhrab_LocationChanged(object sender, EventArgs e)
+        {
+            //int holeX = hole.Location.X;
+            //int holeY = hole.Location.Y;
+
+
+            //int holeX2 = holeX + hole.Size.Width;
+            //int holeY2 = holeY + hole.Size.Height;
+            
+
+        }
+
+        void HitControl(PictureBox pb,PictureBox control,Point point)
+        {
+            int distanceX = pb.Location.X - control.Location.X;
+            int distanceY = pb.Location.Y - control.Location.Y;
+
+            if (Math.Abs(distanceX) < pb.Size.Width && Math.Abs(distanceY) < pb.Size.Height)
+            {
+                pb.Location = point; 
+            }
+        }
+
+        void HitControlForBonus(PictureBox pb)
+        {
+            HitControl(pb, bonus, new Point(pb.Location.X + 100, pb.Location.Y));
+        }
+        void HitControlForHole(PictureBox pb)
+        {
+            HitControl(pb, hole, new Point(12, pb.Location.Y));
+        }
+
+        private void dila_LocationChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            HitControlForBonus(suhrab);
+            HitControlForHole(suhrab);
+            HitControlForHole(dila);
+            HitControlForBonus(dila);
+            HitControlForHole(lokman);
+            HitControlForBonus(lokman);
+            HitControlForHole(mustafa);
+            HitControlForBonus(mustafa);
+            HitControlForHole(mert);
+            HitControlForBonus(mert);
+            HitControlForHole(alpay);
+            HitControlForBonus(alpay);
+            HitControlForHole(senan);
+            HitControlForBonus(senan);
+        }
+    }
+}
